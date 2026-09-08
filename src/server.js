8c34271f29d2b558c4e413504ecd69ed3950bd42
ragini yadav <raginiydv770@gmail.com>
2026-09-08 16:57:37 +0530

    fox port

--- src/server.js
+++ src/server.js
@@ -25,6 +25,6 @@ app.use("/customer", customerRoutes);
 app.use("/customers", customerRoutes);
 app.use("/stats", statsRoutes);
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`ReputeAI API running on port ${PORT}`);
 });
